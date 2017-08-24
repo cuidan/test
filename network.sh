38b7f5410294d191c1624d2f1b1c052a1b44536b
cuidan <1151144794@qq.com>
2017-08-24 12:12:27 +0800

    network.sh

---
+++ network.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+for i in {1..10}
+do 
+	ping -c2 10.0.0.$i &> /dev/null 
+		
+		if  [ $? -eq 1 ]; then
+			echo "10.0.0.$i is down"
+		else
+			echo "10.0.0.$i is ok"
+fi		                          
+done
