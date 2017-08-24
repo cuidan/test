76f172cb8a4ae8eb3d5539cf803e4469385d2684
root <root@cuidan.example.com>
2017-08-24 12:00:20 +0800

    ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -6,7 +6,7 @@ install(){
 [ -x  /usr/bin/ansible ] || install
 
 echo -e "
-[webserver]\nweb-server-1  ansible_ssh_host=10.0.0.194 ansible_ssh_port=22 ansible_user=root ansible_ssh_pass=123456\nweb-server-2  ansible_ssh_host=10.0.0.195 ansible_ssh_port=22 ansible_user=root ansible_ssh_pass=123456\nweb-server-3  ansible_ssh_host=119.23.11.144 ansible_ssh_port=22 ansible_user=root ansible_ssh_pass=cdlz102345-" >>   /etc/ansible/hosts
+[webserver]\nweb-server-1  ansible_ssh_host=10.0.0.194 ansible_ssh_port=22 ansible_user=root ansible_ssh_pass=123456\nweb-server-2  ansible_ssh_host=10.0.0.195 ansible_ssh_port=22 ansible_user=root ansible_ssh_pass=123456" >>   /etc/ansible/hosts
 #ansible webserver -m shell -a "mkdir -p /usr/local/nginx "
 #ansible webserver -m yum -a "name=redis state=latest "
 #ansible webserver -m yum -a 'name="pcre-devel,zlib,zlib-devel,pcre,gcc,gcc-c++" state=latest'
